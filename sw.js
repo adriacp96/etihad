55b558c1bbac22e45e212fe59814a867b4def0b5
Adrià <156062892+adriacp96@users.noreply.github.com>
2026-04-24 11:53:50 +0400

    Add service worker for asset caching
    Implement service worker for caching assets and handling fetch requests.

---
+++ sw.js
@@ -0,0 +1,69 @@
+const CACHE_NAME = 'etihadbus-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn.tailwindcss.com',
+  './assets/fonts/etihad-altis-book-book.woff2',
+  './assets/fonts/etihad-altis-book-book.woff',
+  './assets/fonts/etihad-altis-book-book.ttf',
+  './assets/images/etihadbus.png',
+  './assets/images/etihadlogo.svg',
+  './assets/crewtime.png',
+  './assets/crewsleepy.png',
+  './assets/crewmad.png',
+  './assets/crewlost.png',
+  './assets/crewhappy.png',
+  './assets/crewsun.png',
+  './assets/crewdrink.png',
+  './assets/crewuntie.png',
+  './assets/crewduck.png',
+  './assets/crewexplorer.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('CrewBus: Caching assets');
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      const fetchPromise = fetch(event.request).then((networkResponse) => {
+        if (networkResponse && networkResponse.status === 200) {
+          const responseToCache = networkResponse.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseToCache);
+          });
+        }
+        return networkResponse;
+      }).catch(() => {
+          return cachedResponse;
+      });
+
+      return cachedResponse || fetchPromise;
+    })
+  );
+});
